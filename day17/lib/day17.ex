26f6417c791a88bc24897af8517e872eaab47406
Björn Gustavsson <bgustavsson@gmail.com>
2018-12-17 05:44:58 +0100

    Day 17

---
+++ day17/lib/day17.ex
@@ -0,0 +1,18 @@
+defmodule Day17 do
+  @moduledoc """
+  Documentation for Day17.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Day17.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
