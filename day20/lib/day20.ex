4ae9f6251d7a8992368f41017908c48a7e08b462
Björn Gustavsson <bgustavsson@gmail.com>
2018-12-20 07:01:30 +0100

    WIP

--- day20/lib/day20.ex
+++ day20/lib/day20.ex
@@ -1,6 +1,32 @@
 defmodule Day20 do
-  def part1 re do
-    {:ok, re} = Regex.compile re
-    Regex.match? re, "ENWWWNEEE"
+  def part1 regex do
+    parse_regex regex
+  end
+
+  defp parse_regex <<"^", rest::binary>> do
+    {?\$, <<>>, result} = parse_direction rest, []
+    result
+  end
+
+  defp parse_direction <<char, rest::binary>>, acc do
+    case char do
+      ?N -> parse_direction rest, [:north | acc]
+      ?W -> parse_direction rest, [:west | acc]
+      ?E -> parse_direction rest, [:east | acc]
+      ?S -> parse_direction rest, [:south | acc]
+      ?\( ->
+	{alt, rest} = parse_alt rest, []
+	parse_direction rest, [alt | acc]
+      other ->
+	{other, rest, Enum.reverse(acc)}
+    end
+  end
+
+  defp parse_alt rest, acc do
+    {terminator, rest, alt_group} = parse_direction rest, []
+    case terminator do
+      ?| -> parse_alt rest, [Enum.reverse(alt_group) | acc]
+      ?\) -> {Enum.reverse(acc, [alt_group]), rest}
+    end
   end
 end
--- day20/test/day20_test.exs
+++ day20/test/day20_test.exs
@@ -2,12 +2,16 @@ defmodule Day20Test do
   use ExUnit.Case
   doctest Day20
 
-  test "part one, example" do
-    assert Day20.part1(example1()) == {37, 31, 1147}
+  test "part one, examples" do
+    assert Day20.part1("^WNE$") == 3
+    assert Day20.part1("^ENWWW(NEEE|SSE(EE|N))$") == 10
+    assert Day20.part1("^ENNWSWW(NEWS|)SSSEEN(WNSE|)EE(SWEN|)NNN$") == 18
+    assert Day20.part1("^ESSWWN(E|NNENN(EESS(WNSE|)SSS|WWWSSSSE(SW|NNNE)))$") == 23
+    assert Day20.part1("^WSSEESWWWNW(S|NENNEEEENN(ESSSSW(NWSW|SSEN)|WSWWN(E|WWS(E|SS))))$") == 31
   end
 
   test "part one, real data" do
-    assert Day20.part1(input()) == {937, 628, 588436}
+    #assert Day20.part1(input()) == {937, 628, 588436}
   end
 
   test "part two real data" do
